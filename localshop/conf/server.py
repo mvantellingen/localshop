58f19f62d2d31d0b54356775bef884658d1421af
Michael van Tellingen <michaelvantellingen@gmail.com>
2012-02-05 01:44:33 +0100

    Proper logging for now

--- localshop/conf/server.py
+++ localshop/conf/server.py
@@ -144,24 +144,21 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'root': {
+        'handlers':['console'],
+        'propagate': True,
+        'level':'DEBUG',
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler'
         },
-    }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
 }
 
