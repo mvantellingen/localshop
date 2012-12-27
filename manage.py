6c5ec99427b23f6c64b5a9a36add20f9b8ec0595
Jannis Leidel <jannis@leidel.info>
2012-12-27 19:19:19 +0100

    Re-added localshop.runner module to have a localshop CLI tool as an alternativ to ./manage.py.

--- manage.py
+++ manage.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-import os
-import sys
+from localshop.runner import main
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localshop.settings')
-    os.environ.setdefault('DJANGO_CONFIGURATION', 'Localshop')
-
-    from configurations.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    main()
