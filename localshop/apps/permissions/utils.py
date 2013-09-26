a11a0b753c9f0d254a4051d3fd34809216b4c6d1
Colin Wood <cwood06@gmail.com>
2013-09-26 13:54:04 -0400

    If is a hostname that is verified also verify credentials.

--- localshop/apps/permissions/utils.py
+++ localshop/apps/permissions/utils.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.utils.decorators import available_attrs
 from django.http import HttpResponseForbidden
 
-from localshop.apps.permissions.models import CIDR
+from localshop.apps.permissions.models import CIDR, Hostname
 from localshop.http import HttpResponseUnauthorized
 
 
@@ -40,11 +40,17 @@ def credentials_required(view_func):
     @wraps(view_func, assigned=available_attrs(view_func))
     def decorator(request, *args, **kwargs):
         ip_addr = request.META['REMOTE_ADDR']
+        is_hostname_verified = False
 
         if CIDR.objects.has_access(ip_addr, with_credentials=False):
             return view_func(request, *args, **kwargs)
 
-        if not CIDR.objects.has_access(ip_addr, with_credentials=True):
+        for hostname in Hostname.objects.all():
+            if hostname.is_matching(ip_addr):
+                is_hostname_verified = True
+                break
+
+        if not CIDR.objects.has_access(ip_addr, with_credentials=True) and not is_hostname_verified:
             return HttpResponseForbidden('No permission')
 
         # Just return the original view because already logged in
