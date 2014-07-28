32703a10cc03bccbec2a94c43e4a3a5c05ae0f7f
Dmitry Terrov <terrmit@yandex-team.ru>
2014-07-28 17:04:40 +0400

    Allowed space in package name

--- localshop/apps/packages/urls.py
+++ localshop/apps/packages/urls.py
@@ -6,10 +6,10 @@ from localshop.apps.packages import views
 urlpatterns = patterns('',
     url(r'^$', views.Index.as_view(), name='index'),
 
-    url(r'^(?P<name>[-\._\w]+)/$', views.Detail.as_view(), name='detail'),
+    url(r'^(?P<name>[-\._\w\s]+)/$', views.Detail.as_view(), name='detail'),
 
-    url(r'^(?P<name>[-\._\w]+)/refresh',
+    url(r'^(?P<name>[-\._\w\s]+)/refresh',
         views.refresh, name='refresh'),
-    url(r'^(?P<name>[-\._\w]+)/download/(?P<pk>\d+)/(?P<filename>.*)$',
+    url(r'^(?P<name>[-\._\w\s]+)/download/(?P<pk>\d+)/(?P<filename>.*)$',
         views.download_file, name='download'),
 )
