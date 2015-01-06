mvantellingen/localshop

commit 4c4d6fb1e11437b0baf1f591988bc853784c4a5e

Refactored the setting handling to use django-configurations instead of logan.

localshop/settings.py
@@ -1,0 +1,209 @@
+import os
+import imp
+from celery.schedules import crontab
+
+import djcelery
+djcelery.setup_loader()
+
+from configurations import Settings
+from configurations.utils import uppercase_attributes
+
+
+def FileSettings(path):
+    path = os.path.expanduser(path)
+    mod = imp.new_module('localshop.local')
+    mod.__file__ = path
+
+    try:
+        execfile(path, mod.__dict__)
+    except IOError, e:
+        e.strerror = 'Unable to load configuration file (%s)' % e.strerror
+        raise
+
+    class Holder(object):
+        pass
+
+    for name, value in uppercase_attributes(mod).items():
+        setattr(Holder, name, value)
+
+    return Holder
+
+
+class Base(Settings):
+    # Django settings for localshop project.
+    PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.pardir)
+
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
+
+    ADMINS = (
+        # ('Your Name', 'your_email@example.com'),
+    )
+
+    MANAGERS = ADMINS
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'localshop.db',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
+    }
+
+    # Local time zone for this installation. Choices can be found here:
+    # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+    # although not all choices may be available on all operating systems.
+    # On Unix systems, a value of None will cause Django to use the same
+    # timezone as the operating system.
+    # If running in a Windows environment this must be set to the same as your
+    # system time zone.
+    TIME_ZONE = 'Europe/Amsterdam'
+
+    # Language code for this installation. All choices can be found here:
+    # http://www.i18nguy.com/unicode/language-identifiers.html
+    LANGUAGE_CODE = 'en-us'
+
+    SITE_ID = 1
+
+    # If you set this to False, Django will make some optimizations so as not
+    # to load the internationalization machinery.
+    USE_I18N = True
+
+    # If you set this to False, Django will not format dates, numbers and
+    # calendars according to the current locale.
+    USE_L10N = True
+
+    # If you set this to False, Django will not use timezone-aware datetimes.
+    USE_TZ = True
+
+    LOGIN_URL = '/permissions/login/'
+
+    # Absolute filesystem path to the directory that will hold user-uploaded files.
+    # Example: "/home/media/media.lawrence.com/media/"
+    # MEDIA_ROOT = 'files'
+
+    # Absolute path to the directory static files should be collected to.
+    # Don't put anything in this directory yourself; store your static files
+    # in apps' "static/" subdirectories and in STATICFILES_DIRS.
+    # Example: "/home/media/media.lawrence.com/static/"
+    # STATIC_ROOT = 'assets'
+
+    # URL prefix for static files.
+    # Example: "http://media.lawrence.com/static/"
+    STATIC_URL = '/assets/'
+
+    # Additional locations of static files
+    STATICFILES_DIRS = [
+        os.path.join(PROJECT_ROOT, 'static')
+    ]
+
+    # List of finder classes that know how to find static files in
+    # various locations.
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+
+    # Make this unique, and don't share it with anybody.
+    SECRET_KEY = ''
+
+    # List of callables that know how to import templates from various sources.
+    TEMPLATE_LOADERS = (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    #     'django.template.loaders.eggs.Loader',
+    )
+
+    TEMPLATE_CONTEXT_PROCESSORS = [
+        'django.contrib.auth.context_processors.auth',
+        'django.core.context_processors.debug',
+        'django.core.context_processors.i18n',
+        'django.core.context_processors.media',
+        'django.core.context_processors.static',
+        'django.contrib.messages.context_processors.messages',
+
+        'localshop.apps.packages.context_processors.sidebar',
+    ]
+
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    )
+
+    ROOT_URLCONF = 'localshop.urls'
+
+    # Python dotted path to the WSGI application used by Django's runserver.
+    WSGI_APPLICATION = 'localshop.wsgi.application'
+
+    TEMPLATE_DIRS = (
+        os.path.join(PROJECT_ROOT, 'templates'),
+    )
+
+    BROKER_URL = "django://"
+
+    CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
+
+    CELERYBEAT_SCHEDULE = {
+        # Executes every day at 1:00 AM
+        'every-day-1am': {
+            'task': 'localshop.apps.packages.tasks.update_packages',
+            'schedule': crontab(hour=1, minute=0),
+        },
+    }
+
+    INSTALLED_APPS = [
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.sites',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.admin',
+
+        'kombu.transport.django',
+        'djcelery',
+        'south',
+        'gunicorn',
+
+        'localshop',
+        'localshop.apps.packages',
+        'localshop.apps.permissions',
+    ]
+
+    # A sample logging configuration. The only tangible logging
+    # performed by this configuration is to send an email to
+    # the site admins on every HTTP 500 error when DEBUG=False.
+    # See http://docs.djangoproject.com/en/dev/topics/logging for
+    # more details on how to customize your logging configuration.
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'root': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'handlers': {
+            'console': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler'
+            },
+        },
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            },
+        },
+    }
+
+    LOCALSHOP_DELETE_FILES = False
+
+
+class Localshop(FileSettings('~/.localshop/localshop.conf.py'), Base):
+    pass

localshop/conf/settings.py
@@ -1,8 +1,0 @@
-from localshop.conf.defaults import *
-
-from django.conf import settings
-
-
-for k in dir(settings):
-    if k.startswith('LOCALSHOP_'):
-        locals()[k.split('LOCALSHOP_', 1)[1]] = getattr(settings, k)

.gitignore
@@ -1,1 +1,2 @@
 *.egg-info
+localshop.db

localshop/__init__.py
@@ -1,1 +1,1 @@
-VERSION = '0.1'
+VERSION = __version__ = '0.3'

localshop/apps/packages/models.py
@@ -2,6 +2,7 @@ import os
 import docutils.core
 from docutils.utils import SystemMessage
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_delete
@@ -12,7 +13,6 @@ from model_utils import Choices
 from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 
 from localshop.apps.packages.utils import OverwriteStorage, delete_files
-from localshop.conf import settings
 
 
 class Classifier(models.Model):
@@ -168,6 +168,6 @@ class ReleaseFile(models.Model):
         return '%s#md5=%s' % (url, self.md5_digest)
 
 
-if settings.DELETE_FILES:
+if settings.LOCALSHOP_DELETE_FILES:
     post_delete.connect(delete_files, sender=ReleaseFile,
                         dispatch_uid="localshop.apps.packages.utils.delete_files")

localshop/conf/defaults.py
@@ -1,15 +1,0 @@
-import os
-
-ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir))
-
-# Directory to stash log files in
-LOG_DIR = os.path.join(ROOT, 'log')
-
-# Directory to stash pid files in
-RUN_DIR = os.path.join(ROOT, 'run')
-
-# Web Service
-WEB_HOST = 'localhost'
-WEB_PORT = 8900
-
-DELETE_FILES = False

localshop/conf/server.py
@@ -1,179 +1,0 @@
-import os
-
-from celery.schedules import crontab
-
-import djcelery
-djcelery.setup_loader()
-
-# Django settings for localshop project.
-PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.pardir)
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'localshop.db',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Amsterdam'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-LOGIN_URL = '/permissions/login/'
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-# MEDIA_ROOT = 'files'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-# STATIC_ROOT = 'assets'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/assets/'
-
-# Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static')
-]
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = [
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.contrib.messages.context_processors.messages',
-
-    'localshop.apps.packages.context_processors.sidebar',
-]
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-
-ROOT_URLCONF = 'localshop.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'localshop.wsgi.application'
-
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates'),
-)
-
-BROKER_URL = "django://"
-
-CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
-
-CELERYBEAT_SCHEDULE = {
-    # Executes every day at 1:00 AM
-    'every-day-1am': {
-        'task': 'localshop.apps.packages.tasks.update_packages',
-        'schedule': crontab(hour=1, minute=0),
-    },
-}
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-
-    'djkombu',
-    'djcelery',
-    'south',
-    'gunicorn',
-
-    'localshop',
-    'localshop.apps.packages',
-    'localshop.apps.permissions',
-]
-
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'handlers': ['console'],
-        'propagate': True,
-        'level': 'DEBUG',
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler'
-        },
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-    },
-}

localshop/runner.py
@@ -1,64 +1,16 @@
-from logan.runner import run_app
-from django.utils.crypto import get_random_string
-
-
-def generate_settings():
-    """
-    This command is run when ``default_path`` doesn't exist, or ``init`` is
-    run and returns a string representing the default data to put into their
-    settings file.
-    """
-    CONFIG_TEMPLATE = """
-import os.path
-
-from localshop.conf.server import *
-
-ROOT = os.path.dirname(__file__)
-
-DATABASES = {
-    'default': {
-        # You can swap out the engine for MySQL easily by changing this value
-        # to ``django.db.backends.mysql`` or to PostgreSQL with
-        # ``django.db.backends.postgresql_psycopg2``
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(ROOT, 'localshop.db'),
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
-# Where the packages are stored
-MEDIA_ROOT = os.path.join(ROOT, 'files')
-# *DON'T set MEDIA_URL since we don't want to serve those files directly
-# but only through a view to reduce the chance of a security breach
-
-STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(ROOT, 'assets')
-
-# Comment out the following lines to enable the optional credential system
-# AUTHENTICATION_BACKENDS = [
-#     'django.contrib.auth.backends.ModelBackend',
-#     'localshop.apps.permissions.backend.CredentialBackend',
-# ]
-
-SECRET_KEY = '%(secret_key)s'
-
-    """
-    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    values = {'secret_key': get_random_string(50, chars)}
-    return CONFIG_TEMPLATE % values
+#!/usr/bin/env python
+import os
+import sys
 
 
 def main():
-    run_app(
-        project='localshop',
-        default_config_path='~/.localshop/localshop.conf.py',
-        default_settings='localshop.conf.defaults',
-        settings_initializer=generate_settings,
-        settings_envvar='LOCALSHOP_CONF',
-    )
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localshop.settings')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Localshop')
 
-if __name__ == '__main__':
+    from configurations.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
     main()

commit fc8349b7a156de50fc4b35c73ce966f1616665da

Fix project root in settings
This fixes a bug introduced when refactoring settings in
4c4d6fb1e11437b0baf1f591988bc853784c4a5e.

localshop/settings.py
@@ -38,7 +38,7 @@ def FileSettings(path):
 
 class Base(Settings):
     # Django settings for localshop project.
-    PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.pardir)
+    PROJECT_ROOT = os.path.dirname(__file__)
 
     DEBUG = False
     TEMPLATE_DEBUG = DEBUG