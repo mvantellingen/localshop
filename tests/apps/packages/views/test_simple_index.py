9fdf3f56899b677647ec61bf2bc66c4f27107aa7
Cesar Canassa <cesar.canassa@gmail.com>
2015-01-17 02:51:04 -0200

    Split the tests in two files

--- tests/apps/packages/views/test_simple_index.py
+++ tests/apps/packages/views/test_simple_index.py
@@ -1,15 +1,10 @@
 from base64 import standard_b64encode
 
-from django.core.urlresolvers import reverse
-import mock
 import pytest
 import requests
 
 from localshop.apps.packages.models import Package
-from localshop.apps.packages.views import download_file
 from localshop.apps.permissions.models import CIDR
-
-from tests.apps.packages.factories import ReleaseFileFactory
 
 
 @pytest.mark.parametrize('separator', ['\n', '\r\n'])
@@ -214,46 +209,3 @@ def test_package_register(live_server, admin_user):
     assert release.summary == 'A private pypi server including auto-mirroring of pypi.'
     assert release.user == admin_user
     assert release.version == '0.1'
-
-
-@mock.patch('localshop.apps.packages.tasks.download_file')
-@pytest.mark.django_db
-def test_download_file_with_missing_distribution(download_file_mock, rf):
-    CIDR.objects.create(cidr='0.0.0.0/0', require_credentials=False)
-    release_file = ReleaseFileFactory(distribution=None)
-
-    args = (release_file.release.package.name,
-            release_file.pk,
-            release_file.filename)
-
-    request = rf.get(reverse('packages:download', args=args))
-
-    response = download_file(request, *args)
-
-    # The request is redirected to PyPI
-    assert response.status_code == 302
-    assert response.url == release_file.url
-
-    # The download file task must the queued
-    assert download_file_mock.delay.call_count == 1
-    assert download_file_mock.delay.call_args[1] == {'pk': release_file.pk}
-
-
-@pytest.mark.django_db
-def test_download_file_with_existing_distribution(rf):
-    CIDR.objects.create(cidr='0.0.0.0/0', require_credentials=False)
-    release_file = ReleaseFileFactory()
-
-    args = (release_file.release.package.name,
-            release_file.pk,
-            release_file.filename)
-
-    request = rf.get(reverse('packages:download', args=args))
-
-    response = download_file(request, *args)
-
-    # Localshop must return the release file
-    assert response.status_code == 200
-    assert response.content == 'the file data'
-    assert response.get('Content-Length') == '13'
-    assert response.get('Content-Disposition') == 'attachment; filename=test-1.0.0-sdist.zip'
