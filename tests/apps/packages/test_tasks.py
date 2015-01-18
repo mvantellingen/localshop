fbbd485f8694ff81294999dcec92d4ee8de659d1
Cesar Canassa <cesar.canassa@gmail.com>
2015-01-18 03:19:52 -0200

    Converted tests to test_tasks to py.test

--- tests/apps/packages/test_tasks.py
+++ tests/apps/packages/test_tasks.py
@@ -1,59 +1,51 @@
+from md5 import md5
+
 import mock
-from django.test import TestCase
+import pytest
 
-from localshop.apps.packages import tasks
-from localshop.apps.packages import models
+from localshop.apps.packages import tasks, models
+from tests.apps.packages.factories import ReleaseFileFactory
 
 
-class TestTasks(TestCase):
-    def test_download_file(self):
-        package = models.Package.objects.create(name='localshop')
-        release = models.Release.objects.create(package=package, version='0.1')
-        release_file = models.ReleaseFile.objects.create(
-            release=release,
-            md5_digest='098f6bcd4621d373cade4e832627b4f6',
-            python_version='source',
-            url=(
-                'http://pypi.python.org/packages/source/l/localshop/'
-                'localshop-0.1.tar.gz'
-            )
-        )
+@mock.patch('requests.get')
+@pytest.mark.django_db
+def test_download_file(requests_mock):
+    file_data = 'My cool package'
+    release_file = ReleaseFileFactory(distribution=None,
+                                      md5_digest=md5(file_data).hexdigest())
 
-        with mock.patch('requests.get') as mock_obj:
-            mock_obj.return_value = mock.Mock()
-            mock_obj.return_value.content = 'test'
-            mock_obj.return_value.headers = {
-                'content-length': 1024
-            }
-            tasks.download_file(release_file.pk)
+    requests_mock.return_value = mock.Mock(**{
+            'headers': {
+                'content-length': len(file_data),
+                'content-type': 'application/octet-stream',
+            },
+            'content': file_data,
+        })
 
-        release_file = models.ReleaseFile.objects.get(pk=release_file.pk)
-        self.assertEqual(release_file.distribution.read(), 'test')
+    tasks.download_file.run(release_file.pk)
 
-        self.assertEqual(
-            release_file.distribution.name,
-            'source/l/localshop/localshop-0.1.tar.gz')
+    release_file = models.ReleaseFile.objects.get(pk=release_file.pk)
 
-    def test_download_file_incorrect_md5_sum(self):
-        package = models.Package.objects.create(name='localshop')
-        release = models.Release.objects.create(package=package, version='0.1')
-        release_file = models.ReleaseFile.objects.create(
-            release=release,
-            md5_digest='098f6bcd4621d373cade4e832627b4f6',
-            python_version='source',
-            url=(
-                'http://pypi.python.org/packages/source/l/localshop/'
-                'localshop-0.1.tar.gz'
-            )
-        )
+    assert release_file.distribution.read() == file_data
+    assert release_file.distribution.name == '2.7/t/test-package/test-1.0.0-sdist.zip'
 
-        with mock.patch('requests.get') as mock_obj:
-            mock_obj.return_value = mock.Mock()
-            mock_obj.return_value.content = 'tes.'
-            mock_obj.return_value.headers = {
-                'content-length': 1024
-            }
-            tasks.download_file(release_file.pk)
 
-        release_file = models.ReleaseFile.objects.get(pk=release_file.pk)
-        self.assertFalse(release_file.distribution)
+@mock.patch('requests.get')
+@pytest.mark.django_db
+def test_download_file_incorrect_md5_sum(requests_mock):
+    file_data = 'My cool package'
+    release_file = ReleaseFileFactory(distribution=None, md5_digest='arcoiro')
+
+    requests_mock.return_value = mock.Mock(**{
+            'headers': {
+                'content-length': len(file_data),
+                'content-type': 'application/octet-stream',
+            },
+            'content': file_data,
+        })
+
+    tasks.download_file.run(release_file.pk)
+
+    release_file = models.ReleaseFile.objects.get(pk=release_file.pk)
+
+    assert not release_file.distribution
