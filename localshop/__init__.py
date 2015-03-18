70d335a1b8f96602e7c379652181f5f0218a416a
Cesar Canassa <cesar.canassa@gmail.com>
2015-03-18 17:51:42 -0300

    Version bump

--- localshop/__init__.py
+++ localshop/__init__.py
@@ -1,1 +1,1 @@
-VERSION = __version__ = '0.4.0'
+VERSION = __version__ = '0.6.1'
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open('README.rst', 'r') as fh:
 
 setup(
     name='localshop',
-    version='0.6.0',
+    version='0.6.1',
     author='Michael van Tellingen',
     author_email='michaelvantellingen@gmail.com',
     url='http://github.com/mvantellingen/localshop',
