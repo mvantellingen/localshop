550e096ec442cea91d8f6491364ed6130b8f6449
Michael van Tellingen <michael@mvantellingen.nl>
2015-05-25 15:17:11 +0200

    Update classifiers to mention Python 3 support

--- setup.py
+++ setup.py
@@ -69,6 +69,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
