2f9dc10b98f3b9eaea5f6cfa99e35315889e42dd
Dmitry Terrov <terrmit@yandex.team.ru>
2016-02-26 19:19:27 +0300

    Fix data migration for accounts_user

--- localshop/apps/accounts/migrations/0002_migrate_users.py
+++ localshop/apps/accounts/migrations/0002_migrate_users.py
@@ -4,17 +4,19 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
-
 def forwards(apps, schema_editor):
+    User = apps.get_model('accounts', 'User')
 
     if 'auth_user' in schema_editor.connection.introspection.table_names():
+        db_columns = [f.get_attname_column()[1] for f in User._meta.fields]
+        sql_for_copy_rows = (
+            'INSERT INTO accounts_user ({0}) '
+            'SELECT {0} FROM auth_user '
+            'WHERE id > 0'.format(', '.join(db_columns))
+        )
         cursor = schema_editor.connection.cursor()
-        cursor.execute("""
-            INSERT INTO accounts_user
-            SELECT * FROM auth_user WHERE id > 0
-        """)
+        cursor.execute(sql_for_copy_rows)
 
-    User = apps.get_model('accounts', 'User')
     if User.objects.count() > 0:
         Team = apps.get_model('accounts', 'Team')
         Member = apps.get_model('accounts', 'TeamMember')
