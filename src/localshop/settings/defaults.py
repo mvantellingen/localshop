b18dde5509cc556d36d7b8fb5bde78c062b8a22f
Michael van Tellingen <michael@mvantellingen.nl>
2017-11-18 09:44:46 +0100

    Fix template path in settings

--- src/localshop/settings/defaults.py
+++ src/localshop/settings/defaults.py
@@ -86,7 +86,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(PROJECT_ROOT, 'templates'),
         ],
         'OPTIONS': {
             'context_processors': [
